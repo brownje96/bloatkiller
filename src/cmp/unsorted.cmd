ac20ecd96b23f348e86fff8cda0d002e5c1066d1
brownje2195 <brownje2195@gmail.com>
2022-02-17 08:41:50 -0500

    Add Adobe Crap

--- src/cmp/unsorted.cmd
+++ src/cmp/unsorted.cmd
@@ -5,3 +5,19 @@ net stop bthserv
 
 ECHO ...Intel Killer Networks
 net stop "Killer Analytics Service"
+
+ECHO Kill unsorted processes
+REM may not be great for a graphic designer. TODO possibly add cmd line args in the future
+
+ECHO ...Adobe Processes.
+TASKKILL /F /IM "Creative Cloud.exe"
+TASKKILL /F /IM "Adobe Desktop Service.exe"
+TASKKILL /F /IM "Adobe CEF Helper.exe"
+TASKKILL /F /IM "Creative Cloud Helper.exe"
+TASKKILL /F /IM "CCLibrary.exe"
+TASKKILL /F /IM "acrotray.exe"
+TASKKILL /F /IM "CCXProcess.exe"
+TASKKILL /F /IM "AdobeIPCBroker.exe"
+TASKKILL /F /IM "AdobeUpdateService.exe"
+TASKKILL /F /IM "AdobeAGSService.exe"
+TASKKILL /F /IM "AGMService.exe"
